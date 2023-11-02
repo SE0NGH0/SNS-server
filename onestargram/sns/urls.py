37fc8237934117f1f3bc7dfb1c9b9bb210023892
SE0NGH0 <ericsungho@naver.com>
2023-11-02 11:16:56 +0900

    수정

--- onestargram/sns/urls.py
+++ onestargram/sns/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from . import views
+from . views import *
+
+app_name='sns'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index),
+    path('sns/', sns),
 ]
--- onestargram/sns/views.py
+++ onestargram/sns/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# index.html 페이지를 부르는 index 함수
 def index(request):
     return render(request,'sns/index.html')
+
+# sns.html 페이지를 부르는 blog 함수
+def sns(request):
+    return render(request, 'sns/sns.html')
