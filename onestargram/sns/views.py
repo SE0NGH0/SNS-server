10eeb698141fe6730da3070579dc67bd08fc4407
SE0NGH0 <ericsungho@naver.com>
2023-11-02 15:56:52 +0900

    수정

--- onestargram/sns/views.py
+++ onestargram/sns/views.py
@@ -17,23 +17,16 @@ def sns(request):
 def posting(request, pk):
     # 게시글(Post) 중 pk(primary_key)를 이용해 하나의 게시글(post)를 검색
     post = Post.objects.get(pk=pk)
-    # posting.html ㅠㅔ이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
+    # posting.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
     return render(request, 'sns/posting.html', {'post':post})
 
 def new_post(request):
     if request.method == 'POST':
-        if request.POST['mainphoto']:
-            new_article=Post.objects.create(
-                postname=request.POST['postname'],
-                contents=request.POST['contents'],
-                mainphoto=request.POST['mainphoto'],
-            )
-        else:
-            new_article=Post.objects.create(
-                postname=request.POST['postname'],
-                contents=request.POST['contents'],
-                mainphoto=request.POST['mainphoto'],
-            )
+        new_article=Post.objects.create(
+            postname=request.POST['postname'],
+            mainphoto=request.POST['mainphoto'],
+            contents=request.POST['contents'],
+        )
         return redirect('/sns/')
     return render(request, 'sns/new_post.html')
 
