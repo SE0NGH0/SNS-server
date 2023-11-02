c24d954e772067a304142de8f25f93a259baf92a
SE0NGH0 <ericsungho@naver.com>
2023-11-02 11:48:50 +0900

    수정

--- onestargram/sns/urls.py
+++ onestargram/sns/urls.py
@@ -1,10 +1,16 @@
+from django.contrib import admin
 from django.urls import path
-
-from . views import *
+#index는 대문, blog는 게시판
+from sns.views import index, sns, posting
 
 app_name='sns'
 
 urlpatterns = [
-    path('', index),
-    path('sns/', sns),
+    path('admin/', admin.site.urls),
+    # 웹사이트의 첫화면은 index 페이지이다 + URL 이름은 index이다
+    path('', index, name='index'),
+    # URL:80/sns에 접속하면 sns 페이지 + URL 이름은 sns이다
+    path('sns/', sns, name='sns'),
+    # URL:80/SNS/숫자로 접속하면 게시들-세부페이지(posting)
+    path('sns/<int:pk>', posting, name="posting"),
 ]
--- onestargram/sns/views.py
+++ onestargram/sns/views.py
@@ -12,3 +12,10 @@ def sns(request):
     postlist = Post.objects.all()
     # sns.html 페이지를 열 때, 모든 Post인 postlist도 같이 가져온다
     return render(request, 'sns/sns.html', {'postlist':postlist})
+
+# sns의 게시글(posting)을 부르는 posting 함수
+def posting(request, pk):
+    # 게시글(Post) 중 pk(primary_key)를 이용해 하나의 게시글(post)를 검색
+    post = Post.objects.get(pk=pk)
+    # posting.html ㅠㅔ이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
+    return render(request, 'sns/posting.html', {'post':post})
